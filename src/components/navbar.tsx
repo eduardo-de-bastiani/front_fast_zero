e3fa47ac17aae3f3987509255ab41da035fbe492
Eduardo De Bastiani <eduardocolladebastiani@gmail.com>
2025-02-13 16:12:53 -0300

    changing navbar style

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -23,8 +23,30 @@ const Navbar: React.FC<NavbarProps> = ({username}) => {
   };
 
   return (
-    <AppBar position="fixed" color='primary'>
-      <Toolbar variant='dense'>
+    <AppBar 
+    position="fixed"
+    sx={{ 
+      backgroundColor: 'transparent', 
+      boxShadow: 'none',
+      overflow: 'hidden', // Esconde o overflow do blur
+    }}
+    >
+      {/* Div do fundo com blur */}
+      <div
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          background: 'linear-gradient(90deg, rgba(81, 45, 168, 0.8), rgba(0, 172, 193, 0.8))',
+          backgroundSize: '300% 100%',
+          animation: 'gradientAnimation 10s ease infinite',
+          filter: 'blur(10px)', // Aplicando o blur somente no fundo
+          zIndex: -1, // Coloca o fundo atrás dos outros elementos
+        }}
+      />
+      <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           ToDos' App
         </Typography>
