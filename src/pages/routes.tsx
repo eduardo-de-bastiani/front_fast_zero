2ece708b3dbe26181661b201fcb153d5c7d53ae8
Eduardo De Bastiani <eduardocolladebastiani@gmail.com>
2025-02-15 17:39:17 -0300

    adding login and create_account routes to router

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -3,12 +3,17 @@ import TasksList from "./tasks_list";
 import Home from "./home";
 import AppLayout from "./app_layout";
 import NewTask from "./new_task";
+import Login from "./login";
+import CreateAccount from "./create_account";
+
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
+        <Route path="/login" element={<Login />}/>
+        <Route path="/create_account" element={<CreateAccount />}/>
         <Route path="/app" element={<AppLayout />}>
           <Route index element={<TasksList />} />
           <Route path="new" element={<NewTask />} />
